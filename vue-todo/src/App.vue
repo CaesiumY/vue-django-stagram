4a12454c04eaf22f40418a545c6b33d627428260
caesiumy <mn0658@naver.com>
2019-10-10 21:44:29 +0900

    feat: Add delete item & save local storage

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -7,25 +7,52 @@
     </section>
     <section>
       <ol>
-        <li v-for="(item, index) in list" :key="index">{{item}} 은 {{index + 1}}번째</li>
+        <li v-for="(item, index) in list" :key="index">
+          {{item}} 은 {{index + 1}}번째
+          <button @click="removeItem(index)">삭제</button>
+        </li>
       </ol>
     </section>
   </div>
 </template>
 
 <script>
+const STORAGE_KEY = "todoList";
+
+var storage = {
+  fetch: function() {
+    var todos = localStorage.getItem(STORAGE_KEY) || "[]";
+    todos = JSON.parse(todos);
+    return todos;
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  }
+};
+
 export default {
   name: "app",
   data() {
     return {
       value: "",
-      list: []
+      list: storage.fetch()
     };
+  },
+  watch: {
+    list: {
+      handler: function(todos) {
+        storage.save(todos);
+      },
+      deep: true
+    }
   },
   methods: {
     addItem() {
       this.list.push(this.value);
       this.value = "";
+    },
+    removeItem(index) {
+      this.list.splice(index, 1);
     }
   },
   components: {}
