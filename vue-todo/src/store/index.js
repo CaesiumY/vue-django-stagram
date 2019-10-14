a652dd1789fa84d7e69bbf2a3e91eeb139f3993a
CaesiumY <mn0658@naver.com>
2019-10-14 22:34:22 +0900

    feat: Add vuex store elements

--- vue-todo/src/store/index.js
+++ vue-todo/src/store/index.js
@@ -3,9 +3,32 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const STORAGE_KEY = "todoList";
+
 export default new Vuex.Store({
-    state: {},
-    getters: {},
-    actions: {},
-    mutations: {}
+    state: {
+        list: []
+    },
+    getters: {
+        getList(state) {
+            return state.list;
+        }
+    },
+    actions: {
+        asyncSetList({ commit }, STORAGE_KEY) {
+            let lsList = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+            commit("setList", lsList);
+        }
+    },
+    mutations: {
+        setList(state, payload) {
+            state.list = payload;
+        },
+        pushList(state, payload) {
+            state.list.push(payload);
+        },
+        removeList(state, payload) {
+            state.list.splice(payload, 1);
+        }
+    }
 });
