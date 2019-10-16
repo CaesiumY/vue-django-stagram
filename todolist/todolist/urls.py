b0c7f05abc752cc6c4fd2d97817ab58654ee5f48
CaesiumY <mn0658@naver.com>
2019-10-16 23:47:51 +0900

    feat: Add simple templates & Register admin

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todos.views import TodoView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('todos/', TodoView.as_view()),
 ]
