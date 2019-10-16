80c6ed40feb50f2a5fdff3f9db074cd8bed357b2
CaesiumY <mn0658@naver.com>
2019-10-16 23:31:25 +0900

    feat: Add class based view

--- todolist/todos/views.py
+++ todolist/todos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Todo
 
 # Create your views here.
+
+
+class TodoView(ListView):
+    model = Todo
+    template_name = "index.html"
+    context_object_name = 'todos'
