1711e80871714f28e556f5324c1fdda83f0d22ba
CaesiumY <mn0658@naver.com>
2019-10-22 13:39:22 +0900

    fix: Change v-on value

--- vue-todo/src/components/list.vue
+++ vue-todo/src/components/list.vue
@@ -10,7 +10,7 @@
           class="item no-outline"
           type="text"
           readonly
-          :value="item"
+          :value="item.body"
           ref="item"
           @dblclick="modifyItem(index)"
           @keyup.enter="modifyItem(index)"
